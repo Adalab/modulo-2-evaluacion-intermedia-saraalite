81b8edb114fc3f6fba494f9006ea035b94b98177
saraalite <saraalite@gmail.com>
2020-06-30 09:22:19 +0200

    Add funtions and event listeners

--- exercise.js
+++ exercise.js
@@ -1,1 +1,50 @@
-'use strict'
+"use strict";
+
+//Función que genera número aleatorio
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+const num = getRandomNumber(100);
+
+console.log(num);
+
+//Recogemos valores
+const input = document.querySelector(".js-input");
+const button = document.querySelector(".js-button");
+const clue = document.querySelector(".js-clue");
+const attemptsText = document.querySelector(".js-attempts");
+let attempts = 0;
+
+
+//Función que nos recoge el valor del input
+function updateValue() {
+  let inputValue = input.value;
+  console.log(inputValue);
+  return  parseInt(inputValue);
+}
+
+//Función que compara los números con if
+
+function compareNumbers(){
+    attempts = attempts + 1
+    attemptsText.innerHTML = 'Número de intentos: '+ attempts;
+    const updatedValue = updateValue();
+    if (updatedValue==num){
+        clue.innerHTML= '¡¡¡Has ganado campeona!!!';
+
+    } else if (updatedValue < num) {
+        clue.innerHTML = 'Demasiado bajo';
+
+    }else if (updatedValue > num){
+        clue.innerHTML = 'Demasiado alto';
+    }
+
+}
+
+
+
+
+
+
+button.addEventListener("click",updateValue);
+button.addEventListener("click",compareNumbers);
